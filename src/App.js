880f5fa7edbed6f63ffc2f2e0dc17c66295f3ac6
Dylan Docherty <dylandocherty98@gmail.com>
2020-09-28 16:48:15 -0400

    - started navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import './components/navigation-bar/navigation-bar.component'
+import { NavigationBar } from './components/navigation-bar/navigation-bar.component';
 function App() {
   return (
     <div className="App">
+      <NavigationBar props={["google.com","bungie.net"]}/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/navigation-bar/navigation-bar.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import "../navigation-bar/navigation-bar.style.css";
+
+export const NavigationBar = ({props, handleChange}) => (
+    <div className="navigation-bar">
+        {   props.map(element=>(<a href={element} className="navigation-bar-link">{element}</a>))
+        }
+    </div>
+);
