096f4b8344e3aa52236d615e4b2f28f7afa96c84
Dylan Docherty <dylandocherty98@gmail.com>
2020-10-03 19:44:01 -0400

    -overhauled the styling of my website

--- src/components/content/content.component.jsx
+++ src/components/content/content.component.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 
 import "../content/content.style.css";
 
-export const Content = ({picture, pictureText, pictureStyle, header, text, handleChange}) => (
+export const Content = ({picture, pictureText, pictureStyle, picturePos, header, text, handleChange}) => (
     <div className="content-box">
         <h2 className="header-text">{header}</h2>
         
-            {(typeof picture)==="string" &&
+            {(typeof picture)==="string" && picturePos === "left" &&
                 <div className="body-content">
                     {/* <div className="picture" id={picture}/> */}
                     <img src={picture} alt={pictureText} id={pictureStyle}/>
                     <div className="body-text">    
                         {text}
                     </div>         
+                </div>
+            }
+            {(typeof picture)==="string" && picturePos === "right" &&
+                <div className="body-content">
+                    {/* <div className="picture" id={picture}/> */}
+                    <div className="body-text">    
+                        {text}
+                    </div>         
+                    <img src={picture} alt={pictureText} id={pictureStyle}/>
                 </div>
             }
             {(typeof picture)=== "undefined" &&
--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -9,10 +9,12 @@ export const Home=({props, handleChange})=>{
         
         <div id="main-content">
   
-        <Content id="about-me" picture="https://avatars2.githubusercontent.com/u/33208705?s=460&u=6274729d4a65bdf000c64e4bcbe54704c43328a4&v=4"
+        <Content id="about-me" 
+        picture="https://avatars2.githubusercontent.com/u/33208705?s=460&u=6274729d4a65bdf000c64e4bcbe54704c43328a4&v=4"
         pictureText="A Photo Of Dylan"
         pictureStyle="profilePicture"
-        header="Who is Dylan Docherty?"
+        picturePos="left"
+        header="Dylan Docherty"
         text="
         I am a 22 year old computer scientist and all around tech enthusiast.  
         I have a 4 year degree in Computer Science from the University of Windsor, with a 72% cumulative average.  
@@ -20,6 +22,15 @@ export const Home=({props, handleChange})=>{
         When I code I strive to write efficient, maintainable and readable code.
         I love solving puzzles.
         "/>   
+        <Content id="about-me"
+        picture=""
+        pictureText="A Photo Of Dylan"
+        pictureStyle="profilePicture"
+        picturePos="right"
+        header=""
+        text="During my free time I like to be with my friends, either playing video games or board games.  I like going for longboarding rides with my friends.
+        I also enjoy driving my car, and keeping it in mint condition."
+        />
         </div>
       </div>
     )}
